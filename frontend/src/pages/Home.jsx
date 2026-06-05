5b338d68ea8a9725af6e222cadf10254ecbcca1d
Parvej Khan <iamparvejkhan03@gmail.com>
2026-06-05 14:40:30 +0530

    added lazy and suspense

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,38 @@
 import Container from "../components/Container";
 import { Hero } from "../components";
-import { useState, useEffect, useRef } from 'react';
-import { Coffee } from "lucide-react";
+import { useState, useEffect, useRef, lazy, Suspense } from 'react';
+import { Coffee, Loader } from "lucide-react";
 import { Link } from "react-router-dom";
-import ProblemSection from "../components/ProblemSection";
-import JourneySection from "../components/JourneySection";
-import ComparisonSection from "../components/ComparisonSection";
-import TestimonialsSection from "../components/TestimonialsSection";
-import CTASection from "../components/CTASection";
+
+const ProblemSection = lazy(() => import('../components/ProblemSection'));
+const JourneySection = lazy(() => import('../components/JourneySection'));
+const ComparisonSection = lazy(() => import('../components/ComparisonSection'));
+const TestimonialsSection = lazy(() => import('../components/TestimonialsSection'));
+const CTASection = lazy(() => import('../components/CTASection'));
 
 function Home() {
     return (
         <>
             <Hero />
-            <ProblemSection />
-            <JourneySection />
-            <ComparisonSection />
-            <TestimonialsSection />
-            <CTASection />
+            <Suspense fallback={<Loader className="animate-spin w-full text-center" />}>
+                <ProblemSection />
+            </Suspense>
+
+            <Suspense fallback={<Loader className="animate-spin w-full text-center" />}>
+                <JourneySection />
+            </Suspense>
+
+            <Suspense fallback={<Loader className="animate-spin w-full text-center" />}>
+                <ComparisonSection />
+            </Suspense>
+
+            <Suspense fallback={<Loader className="animate-spin w-full text-center" />}>
+                <TestimonialsSection />
+            </Suspense>
+
+            <Suspense fallback={<Loader className="animate-spin w-full text-center" />}>
+                <CTASection />
+            </Suspense>
         </>
     );
 }
